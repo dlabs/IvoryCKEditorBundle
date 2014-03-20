b2beae4d45a9920ed3bccec6dfd4a6f436009020
Mitja Orlic <mitja.orlic@dlabs.si>
2014-03-20 15:35:38 +0100

    [BOFTWO-860] Added character count plugin

--- Resources/public/config.js
+++ Resources/public/config.js
@@ -3,4 +3,17 @@
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {};
+CKEDITOR.editorConfig = function( config ) {
+    config.wordcount = {
+
+        // Whether or not you want to show the Word Count
+        showWordCount: false,
+
+        // Whether or not you want to show the Char Count
+        showCharCount: true,
+
+        // Whether or not to include Html chars in the Char Count
+        countHTML: false
+    };
+};
+
